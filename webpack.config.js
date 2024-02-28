956be8f11712f0b1b05053ee88bb26fb7dbc4446
MadameMeduse <m1waligora@gmail.com>
2024-02-28 19:32:49 +0100

    fixed scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.html",
   output: {
     filename: "bundle.js",
@@ -12,11 +13,37 @@ module.exports = {
         test: /\.html$/,
         use: ["html-loader"],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              // Output CSS file to the desired location
+              sassOptions: {
+                outputStyle: "expanded",
+                includePaths: ["./src/scss"],
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, "docs"), // Serve files from 'docs' directory
-    index: "index.html", // Use 'index.html' as the default index file
-    port: 8080, // Optional: Specify the port to listen on
+    static: {
+      publicPath: "/",
+      directory: path.join(__dirname, "docs"), // Serve files from the 'dist' directory
+    }, // index: "index.html", // Use 'index.html' as the default index file
+    port: 3000, // Optional: Specify the port to listen on
   },
 };
